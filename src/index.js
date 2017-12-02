7515e3f3e243407829edadba19401fa7f5ff012a
Michael Friedman <contact@michaeldavidfriedman.com>
2017-12-02 14:42:42 -0800

    create first Redux selector

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import store from "./store";
+import { getReadableStories } from "./selectors/story";
+import { STORY_ARCHIVE } from "./constants/actionTypes";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <App stories={store.getState().storyState} onArchive={() => {}} />,
+  <App
+    stories={getReadableStories(store.getState())}
+    onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
+  />,
   document.getElementById("root"),
 );
 registerServiceWorker();
